a97339ae4941221a0b3fa902f8238fc3b000a0ef
qwelord <secondofall22@rambler.ru>
2025-11-24 17:56:18 +0500

    Добавьте файлы проекта.

---
+++ ApplicationSettings_Kazakov/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ApplicationSettings_Kazakov
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public enum pages{
+            settings
+        }
+
+        public void OpenPages(pages _pages)
+        {
+            if (_pages == pages.settings)
+                frame.Navigate(new Pages().Settings(this));
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            OpenPages(pages.settings);
+        }
+    }
+}
